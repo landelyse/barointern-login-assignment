41dda85ccc6babf59fbf83e5e609af100626027f
jbnong07 <jbnong07@naver.com>
2025-06-10 05:50:13 +0900

    style: change folder and apply swiftlint autocorrect
    폴더 뼈대 생성 및 swiftlint 자동 수정 적용

--- login-assignment/login-assignment/AppLayer/SceneDelegate.swift
+++ login-assignment/login-assignment/AppLayer/SceneDelegate.swift
@@ -11,13 +11,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(
+        _ scene: UIScene,
+        willConnectTo session: UISceneSession,
+        options connectionOptions: UIScene.ConnectionOptions
+    ) {
         guard let windowScene: UIWindowScene = (scene as? UIWindowScene) else { return }
         let window: UIWindow = UIWindow(windowScene: windowScene)
         window.rootViewController = ViewController()
         window.makeKeyAndVisible()
-        
+
         self.window = window
     }
 
@@ -36,6 +39,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func sceneDidEnterBackground(_ scene: UIScene) {
     }
 
-
 }
-
