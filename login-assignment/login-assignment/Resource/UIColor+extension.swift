39c64a47fbd9a22b7e7a446dd996e8439c7c4dc8
jbnong07 <jbnong07@naver.com>
2025-06-11 17:23:49 +0900

    feature: define ui color UI에 사용될 색상 추가 UIColor에 확장으로 추가

---
+++ login-assignment/login-assignment/Resource/UIColor+extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIColor.swift
+//  login-assignment
+//
+//  Created by 박진홍 on 6/11/25.
+//
+
+import UIKit
+
+extension UIColor {
+    static let boxColor = UIColor(red: 243, green: 221, blue: 203, alpha: 1)
+    static let buttonColor = UIColor(red: 235, green: 199, blue: 173, alpha: 1)
+    static let accentTextColor = UIColor(red: 143, green: 112, blue: 87, alpha: 1)
+}
