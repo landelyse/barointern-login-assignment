f56b9108ab1186fa1d20a6656609444b04516e3a
jbnong07 <jbnong07@naver.com>
2025-06-12 03:30:51 +0900

    feature: implement StartViewModel StartViewModel 구현

--- login-assignment/login-assignment/Presentation/StartView/StartViewModel.swift
+++ login-assignment/login-assignment/Presentation/StartView/StartViewModel.swift
@@ -5,3 +5,23 @@
 //  Created by 박진홍 on 6/11/25.
 //
 
+import Combine
+import Foundation
+
+@MainActor
+final class StartViewModel {
+    private var cancellables: Set<AnyCancellable> = Set<AnyCancellable>()
+    
+    private let navigateSubject: PassthroughSubject<Void, Never> = PassthroughSubject<Void, Never>()
+    var navigatePublisher: AnyPublisher<Void, Never> {
+        navigateSubject.eraseToAnyPublisher()
+    }
+    
+    init() {
+        
+    }
+    
+    func startButtonTapped() {
+        navigateSubject.send(())
+    }
+}
