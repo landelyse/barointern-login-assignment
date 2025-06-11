6613cc3213425e07e519cff99e4cb63dfcffbe32
jbnong07 <jbnong07@naver.com>
2025-06-11 17:18:13 +0900

    feature: add UIMetric file UI에 사용될 상수 정의

---
+++ login-assignment/login-assignment/Resource/UIMetric.swift
@@ -0,0 +1,28 @@
+//
+//  UIMetric.swift
+//  login-assignment
+//
+//  Created by 박진홍 on 6/11/25.
+//
+
+import Foundation
+
+enum UIMetric {
+    enum Padding {
+        static let small: CGFloat = 8
+        static let regular: CGFloat = 16
+        static let large: CGFloat = 24
+    }
+    
+    enum CornerRadius {
+        static let regular: CGFloat = 15
+    }
+    
+    enum FontSize {
+        static let title: CGFloat = 28
+        static let title2: CGFloat = 24
+        static let subTitle: CGFloat = 20
+        static let body: CGFloat = 16
+        static let caption: CGFloat = 12
+    }
+}
